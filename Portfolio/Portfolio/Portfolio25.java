679f15039628b8a8314c1cda0c652ff2ea8f7779
Guillermo Santana Padrón <lin2studio@gmail.com>
2022-06-22 13:29:24 +0100

    inicio de prueba 25, determinador de ganador en arraylist de partidas de piedra papel tijeras

---
+++ Portfolio/Portfolio/Portfolio25.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import javax.swing.*;
+
+public class Portfolio25 {
+    public static void main(String[] args) {
+
+    }
+}
+class Game{
+    private char Player1;
+    private char Player2;
+
+    public Game(){
+
+    }
+    //setters
+    public void setPlayer1(char player1) {
+        this.Player1 = player1;
+    }
+
+    public void setPlayer2(char player2) {
+        this.Player2 = player2;
+    }
+    //getters
+    public char getPlayer1() {
+        return Player1;
+    }
+
+    public char getPlayer2() {
+        return Player2;
+    }
+    //generador de partida.
+    public void setGame(){
+        
+    }
+}
+
