92eabc2fc0248cf94d81e82a06d8bfbec58883db
Guillermo Santana Padrón <lin2studio@gmail.com>
2022-06-22 15:41:03 +0100

    generador de valores aleatorios 0, 1, 2 -> R, P, S

--- Portfolio/Portfolio/Portfolio25.java
+++ Portfolio/Portfolio/Portfolio25.java
@@ -17,7 +17,6 @@ class Game{
     public void setPlayer1(char player1) {
         this.Player1 = player1;
     }
-
     public void setPlayer2(char player2) {
         this.Player2 = player2;
     }
@@ -25,13 +24,24 @@ class Game{
     public char getPlayer1() {
         return Player1;
     }
-
     public char getPlayer2() {
         return Player2;
     }
     //generador de partida.
     public void setGame(){
-        
+       int tirada = (int)(Math.round((Math.random()*2)%3));
+       char jugada;
+       switch (tirada){
+           case 0:
+               jugada ='R';
+               break;
+           case 1:
+               jugada ='P';
+               break;
+           case 2:
+               jugada ='S';
+               break;
+       }
     }
 }
 
